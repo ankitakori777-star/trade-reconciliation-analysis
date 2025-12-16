905f5e51b51fa178d0ad581edfade33427afa515
ankitakori777-star <ankitakori777@gmail.com>
2025-12-16 20:53:18 +0530

    Update reconciliation_queries.sql

--- sql/reconciliation_queries.sql
+++ sql/reconciliation_queries.sql
@@ -1,1 +1,28 @@
+-- Identify missing trades
+SELECT 
+    fo.trade_id
+FROM front_office fo
+LEFT JOIN back_office bo
+  ON fo.trade_id = bo.trade_id
+WHERE bo.trade_id IS NULL;
+
+-- Identify notional mismatches
+SELECT 
+    fo.trade_id,
+    fo.notional AS fo_notional,
+    bo.notional AS bo_notional
+FROM front_office fo
+JOIN back_office bo
+  ON fo.trade_id = bo.trade_id
+WHERE fo.notional <> bo.notional;
+
+-- Identify currency mismatches
+SELECT 
+    fo.trade_id,
+    fo.currency AS fo_currency,
+    bo.currency AS bo_currency
+FROM front_office fo
+JOIN back_office bo
+  ON fo.trade_id = bo.trade_id
+WHERE fo.currency <> bo.currency;
 
